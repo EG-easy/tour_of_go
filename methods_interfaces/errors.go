48c80031f91cd7d4a9e6641016ec704a36686ee6
EG-easy <1018eguchi@gmail.com>
2018-09-03 15:46:21 +0900

    add new file

---
+++ methods_interfaces/errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+type MyError struct{
+	when time.Time
+	what string
+}
+
+func (e *MyError)Error()string{
+	return fmt.Sprintf("at %v, %s", e.when, e.what)
+}
+
+func run ()error{
+	return &MyError{
+		time.Now(),
+		"it didnot work",
+	}
+}
+
+func main(){
+	if err := run(); err !=nil{
+		fmt.Println(err)
+	}
+}
+
+
+
+
